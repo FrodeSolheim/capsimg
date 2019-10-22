1713452be0c5adf94fcb22bcb083adc5dac45876
Frode Solheim <frode@solheim.dev>
2019-10-22 21:25:44 +0200

    Build script update

--- Makefile.fs
+++ Makefile.fs
@@ -1,45 +1,101 @@
 version = $(shell cat VERSION.fs)
 uname := $(shell uname -a)
 ifneq ($(findstring Msys,$(uname)),)
-os = Windows
-arch = x86_64
-exe = .exe
-dll = .dll
-else ifneq ($(findstring Darwin,$(uname)),)
-os = macOS
-arch = x86_64
-exe =
-dll = .so
+os := Windows
+arch := x86-64
+exe := .exe
+dll := .dll
+opengl_libs := -lopengl32
 else
-os = Linux
-arch = x86_64
-exe =
-dll = .so
+ifneq ($(findstring Darwin,$(uname)),)
+os := macOS
+arch := x86-64
+exe :=
+dll := .so
+opengl_libs := -framework OpenGL
+else
+os := Linux
+arch := x86-64
+exe :=
+dll := .so
+opengl_libs := -lGL
+endif
 endif
 
-all: build
-	@echo Built ${version} for ${os}-${arch}
+plugin_dir = dist.fs/${plugin}
+data_dir = dist.fs/${plugin}/Data
+licenses_dir = dist.fs/${plugin}/Licenses
+locale_dir = dist.fs/${plugin}/Locale
+os_arch_dir = dist.fs/${plugin}/${os}/${arch}
 
-install: plugin-clean plugin-build
-	rm -Rf ../OpenRetro/System/Plugins/${name}
+glib_cflags = $(shell pkg-config --cflags glib-2.0)
+glib_libs = $(shell pkg-config --libs glib-2.0)
+
+cppflags = -DFSGS -DFSEMU -DFSE ${glib_cflags}
+libs = ${glib_libs} ${opengl_libs}
+libs += -lpng -lSDL2_ttf -lsamplerate -lGLEW
+
+fsemu-all: fsemu-build
+	@echo Built ${version} for ${os}_${arch}
+
+.PHONY: assemble bootstrap build clean configure distclean install strip \
+	package plugin plugin-noclean rebuild
+
+assemble: fsemu-assemble
+bootstrap: fsemu-bootstrap
+build: fsemu-build
+clean: fsemu-clean
+configure: fsemu-configure
+distclean: fsemu-distclean
+install: fsemu-install
+strip: fsemu-strip
+package: fsemu-package
+plugin: fsemu-plugin
+plugin-noclean: fsemu-plugin-noclean
+rebuild: fsemu-rebuild
+
+fsemu-rebuild: fsemu-bootstrap fsemu-configure fsemu-clean fsemu-build
+
+fsemu-install: fsemu-build
+	rm -Rf ../OpenRetro/System/Plugins/${plugin}
 	mkdir -p ../OpenRetro/System/Plugins
-	mv dist/${name} ../OpenRetro/System/Plugins/
+	mv dist.fs/${plugin} ../OpenRetro/System/Plugins/
 
-plugin: plugin-clean plugin-build plugin-strip plugin-pack
+fsemu-assemble-pre:
+	rm -Rf ${plugin_dir}
+	mkdir -p ${plugin_dir}
+	echo "[plugin]" > ${plugin_dir}/Plugin.ini
+	echo "name = ${plugin}" >> ${plugin_dir}/Plugin.ini
+	echo "version = ${version}" >> ${plugin_dir}/Plugin.ini
 
-plugin-clean:
-	rm -Rf dist/${name}
+fsemu-assemble-wrap: fsemu-assemble-pre fsemu-assemble
 
-plugin-pack:
-	cd dist && tar cfJ ${name}-${version}-${os}-${arch}.tar.xz ${name}
+fsemu-plugin-noclean: fsemu-build fsemu-assemble-wrap fsemu-strip fsemu-package
+
+fsemu-plugin: fsemu-bootstrap fsemu-configure fsemu-clean fsemu-plugin-noclean
+
+# fsemu-plugin-clean:
+# 	rm -Rf dist.fs/${plugin}
+
+fsemu-package:
+	cd dist.fs && tar cfJ ${plugin}_${version}_${os}_${arch}.tar.xz ${plugin}
 	@echo Packaged ${version} for ${os}-${arch}
 
-plugin-strip:
-	./strip.fs dist/${name}
+fsemu-strip:
+	# ./strip.fs dist.fs/${plugin}
+	./standalone.fs ${os_arch_dir}
 
-name = CAPSImg
+# ----------------------------------------------------------------------------
 
-build:
+plugin = CAPSImg
+
+fsemu-bootstrap:
+	./bootstrap.fs
+
+fsemu-configure:
+	./configure.fs
+
+fsemu-build:
 	make -C CAPSImg
 ifeq ($(os), Windows)
 	cp CAPSImg/CAPSImg.dll capsimg.dll
@@ -49,18 +105,25 @@ else
 	cp CAPSImg/libcapsimage.so.5.1 capsimg.so
 endif
 
-clean:
+fsemu-clean:
 	make -C CAPSImg clean
-	rm -Rf capsimg${dll} dist
+	rm -Rf capsimg${dll} dist.fs
 
-distclean: clean
+fsemu-distclean: clean
 	make -C CAPSImg distclean
 
-plugin-build:
-	mkdir -p dist/${name}/${os}/${arch}
-	cp README.fs dist/${name}/ReadMe.txt
-	echo ${version} > dist/${name}/Version.txt
-	echo ${version} > dist/${name}/${os}/${arch}/Version.txt
-	cp capsimg${dll} dist/${name}/${os}/${arch}/
-	mkdir -p dist/${name}/Licences
-	cp LICENCE.txt dist/${name}/Licences/CAPSImg.txt
+fsemu-assemble:
+	mkdir -p ${plugin_dir}
+	echo ${version} > ${plugin_dir}/Version.txt
+	cp README.fs ${plugin_dir}/ReadMe.txt
+
+	mkdir -p ${os_arch_dir}
+	echo ${version} > ${os_arch_dir}/Version.txt
+	cp capsimg${dll} ${os_arch_dir}
+
+	mkdir -p ${licenses_dir}
+	cp LICENCE.txt ${licenses_dir}/CAPSImg.txt
+
+# Override the default strip target
+fsemu-strip:
+	./strip.fs dist.fs/${plugin}
